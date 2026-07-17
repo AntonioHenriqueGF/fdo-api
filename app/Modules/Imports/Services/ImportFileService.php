85ca362490acdeb78770b65318be8f79e29b8a53
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-07-17 15:34:30 -0300

    refactor: Rename and enhance transaction and balance extraction logic in ImportFileService

--- app/Modules/Imports/Services/ImportFileService.php
+++ app/Modules/Imports/Services/ImportFileService.php
@@ -31,48 +31,57 @@ class ImportFileService
             }
 
             $importId = $this->importsModel->saveImport($userId, $data['fileName'], $binHex);
-            $separatedData = $this->separateTransactionsAndBalances($data['normalized']);
+            $separatedData = $this->extractTransactionsAndBalances($data['normalized']);
 
             $this->dailyBalancesModel->saveDailyBalances($userId, $importId, $separatedData['dailyBalances']);
             $this->transactionsModel->saveTransactions($userId, $importId, $separatedData['singleValueTransactions']);
         });
     }
 
-    public function separateTransactionsAndBalances(array $normalizedData): array
+    public function extractTransactionsAndBalances(array $normalizedData): array
     {
-        $singleValueTransactions = [];
-        $dailyBalances = [];
+        $transactions = [];
+        $dailyBalancesByDate = [];
 
         foreach ($normalizedData as $item) {
-            if (isset($item['amount']) || isset($item['credit_only']) || isset($item['debit_only'])) {
-                $singleValueAmount = $item['amount'] ?? 0;
 
-                if (isset($item['credit_only']) && $item['credit_only'] != 0 && $singleValueAmount == 0) {
-                    $singleValueAmount = $item['credit_only'];
-                }
+            if (isset($item['amount'])) {
+                $amount = $item['amount'];
+            } elseif (isset($item['credit_only'])) {
+                $amount = $item['credit_only'];
+            } elseif (isset($item['debit_only'])) {
+                $amount = $item['debit_only'] > 0 ? -$item['debit_only'] : $item['debit_only'];
+            } else {
+                $amount = null;
+            }
 
-                if (isset($item['debit_only']) && $item['debit_only'] != 0 && $singleValueAmount == 0) {
-                    $singleValueAmount = $item['debit_only'];
-                }
-
-                $singleValueTransactions[] = [
-                    'amount' => $singleValueAmount,
+            if ($amount !== null) {
+                $transactions[] = [
+                    'amount' => $amount,
                     'date_yyyymmdd' => $item['date_yyyymmdd'] ?? null,
                     'description' => $item['description'] ?? null,
+                    'internal_code' => $item['internal_code'] ?? null,
                 ];
             }
 
-            if (isset($item['closing_balance']) && $item['closing_balance'] != 0) {
-                $dailyBalances[] = [
-                    'closing_balance' => $item['closing_balance'],
-                    'date_yyyymmdd' => $item['date_yyyymmdd'] ?? null,
-                ];
+            // Some banks provide daily balances in the same row as the transaction data, so we need to extract them separately.
+            // We will use the date as the key to ensure that we only keep one daily balance per date.
+            // If there is more than one daily balance for a given date, we will keep the last one we encounter in the data, which is usually the most recent one.
+            if (isset($item['closing_balance'])) {
+                $date = $item['date_yyyymmdd'] ?? null;
+
+                if ($date !== null) {
+                    $dailyBalancesByDate[$date] = [
+                        'date_yyyymmdd' => $date,
+                        'closing_balance' => $item['closing_balance'],
+                    ];
+                }
             }
         }
 
         return [
-            'singleValueTransactions' => $singleValueTransactions,
-            'dailyBalances' => $dailyBalances,
+            'singleValueTransactions' => $transactions,
+            'dailyBalances' => array_values($dailyBalancesByDate),
         ];
     }
 }
