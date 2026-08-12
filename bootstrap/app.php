3013c2f117a4ef1c28ba155b037e031b24466e69
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-08-11 22:56:15 -0300

    refactor: Remove unused authentication and exception handling imports in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Response;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
